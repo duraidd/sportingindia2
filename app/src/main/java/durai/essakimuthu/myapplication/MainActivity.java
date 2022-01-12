2eaeb26bdcdd9a7dba232242f5ed11ccb60b1adc
somaiyappan <somasundaram9997@gmail.com>
2022-01-12 12:37:06 +0530

    v1

---
+++ app/src/main/java/durai/essakimuthu/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package durai.essakimuthu.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//  set status text dark
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this,R.color.white));// set status background white
+    }
+}
